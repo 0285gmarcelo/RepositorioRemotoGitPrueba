8e5e7e0bc5bc4445e67a418852d230923f3257ed
isard <isard@DESKTOP-P0UA02U>
2026-03-05 19:39:35 +0100

    Creación de nuevos paquetes.

---
+++ Proyectolocal/src/paqueteRamaprueba/Ramaprueba.java
@@ -0,0 +1,10 @@
+package paqueteRamaprueba;
+
+public class Ramaprueba {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Clase nueva de otra rama.");
+	}
+
+}
