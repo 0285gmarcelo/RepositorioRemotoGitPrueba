fed7ce1d5413ef25eceaf9b86e829a4ea3a8ca18
isard <isard@DESKTOP-P0UA02U>
2026-03-05 16:52:02 +0100

    Mensaje para confirmar los cambios del Proyecto

--- Proyectolocal/src/paquetelocal/LocalMain.java
+++ Proyectolocal/src/paquetelocal/LocalMain.java
@@ -4,7 +4,7 @@ public class LocalMain {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Creando Repositorios Locales con Git");
+		System.out.println("Creando Repositorios Locales con Git.");
 	}
 
 }
